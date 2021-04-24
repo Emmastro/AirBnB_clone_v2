da4fb0be3d2e13ce4e46afe4ad6a1be110f978f2
Emmanuel Murairi <emurairi18@alastudents.org>
2021-04-24 16:53:16 +0400

    install nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
+
+#install nginx web server 
+apt-get update
+apt-get install -y nginx
+
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
@@ -9,11 +14,18 @@ ln -sfn /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/ 
 
 # Add static location to nginx settings: 
-sed -i '50 a \
-location /hbnb_static/ {\
-                alias /data/web_static/current/;\
-                autoindex off;\
-        }\
-' /etc/nginx/sites-enabled/default
+
+printf %s "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        add_header X-Served-By $HOSTNAME;
+        root /var/www/html;
+        index index.html;
+        
+        location /hbnb_static {
+            alias /data/web_static/current;
+            index index.html index.htm;
+        }
+}" > /etc/nginx/sites-available/default
 
 service nginx restart
